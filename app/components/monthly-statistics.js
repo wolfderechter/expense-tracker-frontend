de4ec847459fd18f63214ddca10a3318b3ac7ed1
wolfez <wolfderechter@gmail.com>
2023-08-03 15:19:54 +0200

    add chart

--- app/components/monthly-statistics.js
+++ app/components/monthly-statistics.js
@@ -1,18 +1,65 @@
 import Component from '@glimmer/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class MonthlyStatisticsComponent extends Component {
-  @computed('args.filteredExpenses')
+  @service store;
+  @tracked chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  @computed('args.filteredExpenses.length')
   get totalExpenses() {
     return this.args.filteredExpenses.length;
   }
 
-  @computed('args.filteredExpenses')
+  @computed('args.filteredExpenses', 'totalExpenses')
   get averageValue() {
-    const totalValue = this.args.filteredExpenses.reduce((acc, expense) => {
-      return acc + parseFloat(expense.value);
+    const totalValue = this.args.filteredExpenses.reduce((sum, expense) => {
+      return sum + parseFloat(expense.value);
     }, 0);
     const average = totalValue / this.totalExpenses;
     return isNaN(average) ? 0 : average.toFixed(2);
+  }
+
+  get categoriesByValueData() {
+    const { year, month, filteredExpenses } = this.args;
+    const data = {};
+
+    filteredExpenses.forEach((expense) => {
+      // add default 'no category' if the expense doesn't have a category
+      const categoryTitle = expense.category.get('title') || 'No category';
+      const expenseDate = new Date(expense.date);
+
+      if (
+        month === -1 ||
+        (expenseDate.getFullYear() === year && expenseDate.getMonth() === month)
+      ) {
+        data[categoryTitle] =
+          (data[categoryTitle] || 0) + parseFloat(expense.value);
+      }
+    });
+    return {
+      labels: Object.keys(data),
+      datasets: [
+        {
+          data: Object.values(data),
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#4BC0C0',
+            '#FF9F40',
+            '#9966FF',
+            '#F76D57',
+            '#2DD3FF',
+            '#FFC94D',
+            '#A76DFF',
+          ],
+        },
+      ],
+    };
   }
 }
